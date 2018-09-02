94bec2f62deb07e3519993d3f26339808b138856
Gergely Imreh <imrehg@gmail.com>
2018-09-02 21:35:13 +0100

    add local score storage
    This should limit the amount of API calls required for filling out the page.

--- contentScript.js
+++ contentScript.js
@@ -1,59 +1,86 @@
-chrome.storage.sync.get(
-  {
-    apikey: ""
-  },
-  function(items) {
-    const apikey = items.apikey;
-    if (apikey !== "") {
-      var xhr = new XMLHttpRequest();
-      const all = document.getElementsByClassName(
-        "full-width-tile--now-showing"
-      );
-      for (var i = 0, max = all.length; i < max; i++) {
-        const item = all[i];
-        console.log(item);
-        const title = item.getElementsByClassName("full-width-tile__title")[0]
-          .innerHTML;
-        const countryYear = item
-          .getElementsByClassName(
-            "now-showing-tile-director-year__year-country"
-          )[0]
-          .innerHTML.split(",");
-        const year = countryYear[1].trim();
-        console.log(`${title} ${year}`);
-        const moviekey = `${title}/${year}`;
-        const encodedTitle = encodeURIComponent(title);
-        xhr.open(
-          "GET",
-          `https://www.omdbapi.com/?t=${encodedTitle}&y=${year}&apikey=${apikey}`,
-          false
+function main() {
+  chrome.storage.sync.get(
+    {
+      apikey: ""
+    },
+    function(items) {
+      const apikey = items.apikey;
+      if (apikey !== "") {
+        const all = document.getElementsByClassName(
+          "full-width-tile--now-showing"
         );
-        xhr.send();
-        var result = JSON.parse(xhr.responseText);
-        console.log(result.Ratings);
-        if (result.Ratings && result.Ratings.length > 0) {
-          var div = document.createElement("div");
-          div.setAttribute("class", "ratings");
-          div.innerHTML = '<span class="ratings-header">Ratings:</span>';
-          for (
-            var j = 0, numratings = result.Ratings.length;
-            j < numratings;
-            j++
-          ) {
-            var rating = document.createElement("div");
-            rating.innerHTML = `<span class="rating-source">${
-              result.Ratings[j].Source
-            }</span>: <span class="rating-value">${
-              result.Ratings[j].Value
-            }</span>`;
-            div.appendChild(rating);
-          }
-          var target = item.getElementsByClassName(
-            "full-width-tile__our-take"
-          )[0];
-          target.appendChild(div);
+        for (var i = 0, max = all.length; i < max; i++) {
+          const item = all[i];
+          // console.log(item);
+          const title = item.getElementsByClassName("full-width-tile__title")[0]
+            .innerHTML;
+          const countryYear = item
+            .getElementsByClassName(
+              "now-showing-tile-director-year__year-country"
+            )[0]
+            .innerHTML.split(",");
+          const year = countryYear[1].trim();
+          getRating(title, year, item, apikey);
         }
       }
     }
+  );
+}
+
+function getRating(title, year, item, apikey) {
+  const moviekey = `${title} / ${year}`;
+  var query = {};
+  query[moviekey] = "";
+  chrome.storage.local.get(query, function(items) {
+    if (items[moviekey] === "") {
+      console.log(`No rating found for ${moviekey} in local storage.`);
+      const encodedTitle = encodeURIComponent(title);
+      var xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        `https://www.omdbapi.com/?t=${encodedTitle}&y=${year}&apikey=${apikey}`,
+        false
+      );
+      xhr.send();
+      var result = JSON.parse(xhr.responseText);
+      if (
+        result.Response !== "False" &&
+        result.Ratings &&
+        result.Ratings.length > 0
+      ) {
+        console.log(result.Ratings);
+        var ratingStored = {};
+        ratingStored[moviekey] = result.Ratings;
+        chrome.storage.local.set(ratingStored, function() {
+          console.log(
+            "Value is set to " + JSON.stringify(ratingStored, null, 2)
+          );
+        });
+        showRating(result.Ratings, item);
+      }
+    } else {
+      console.log(`Rating found for ${moviekey} in local storage!`);
+      showRating(items[moviekey], item);
+    }
+  });
+}
+
+function showRating(ratings, item) {
+  console.log(ratings);
+  if (ratings !== null) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "ratings");
+    div.innerHTML = '<span class="ratings-header">Ratings:</span>';
+    for (var j = 0, numratings = ratings.length; j < numratings; j++) {
+      var rating = document.createElement("div");
+      rating.innerHTML = `<span class="rating-source">${
+        ratings[j].Source
+      }</span>: <span class="rating-value">${ratings[j].Value}</span>`;
+      div.appendChild(rating);
+    }
+    var target = item.getElementsByClassName("full-width-tile__our-take")[0];
+    target.appendChild(div);
   }
-);
+}
+
+main();
